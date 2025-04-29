c80ec6812e18face4ea35019408d0421e9089f3f
javahuhu <cgelera3@gmail.com>
2025-04-30 00:17:01 +0800

    add more nlp symptoms

--- petsymp/lib/SymptomQuestions/NLPSymptom.dart
+++ petsymp/lib/SymptomQuestions/NLPSymptom.dart
@@ -1,25 +1,157 @@
 final Map<String, String> symptomNLPMap = {
+
   // Vomiting
   "throwing up": "vomiting",
   "puking": "vomiting",
   "regurgitating": "vomiting",
   "upset stomach": "vomiting",
+  "nauseated": "vomiting",
 
   // Diarrhea
   "loose stools": "diarrhea",
   "watery poop": "diarrhea",
+  "runny stool": "diarrhea",
+  "frequent pooping": "diarrhea",
+  "messy poop": "diarrhea",
 
   // Lethargy
   "tired": "lethargy",
   "low energy": "lethargy",
   "sluggish": "lethargy",
+  "sleepy all the time": "lethargy",
+  "no energy": "lethargy",
 
   // Loss of appetite
   "not eating": "loss of appetite",
   "refusing food": "loss of appetite",
   "no appetite": "loss of appetite",
+  "skipping meals": "loss of appetite",
+  "ignoring food": "loss of appetite",
 
   // Coughing
   "hacking": "coughing",
   "dry cough": "coughing",
+  "persistent cough": "coughing",
+  "honking cough": "coughing",
+  "barking cough": "coughing",
+
+  // Fever
+  "hot body": "fever",
+  "warm nose": "fever",
+  "high temperature": "fever",
+  "feels feverish": "fever",
+  "hot ears": "fever",
+
+  // Sneezing
+  "sneezy": "sneezing",
+  "keeps sneezing": "sneezing",
+  "frequent sneezing": "sneezing",
+  "snorting": "sneezing",
+  "sudden sneezes": "sneezing",
+
+  // Nasal discharge
+  "runny nose": "nasal discharge",
+  "nose dripping": "nasal discharge",
+  "wet nose": "nasal discharge",
+  "bloody nose": "nasal discharge",
+  "mucus from nose": "nasal discharge",
+
+  // Seizures
+  "convulsions": "seizures",
+  "fits": "seizures",
+  "twitching body": "seizures",
+  "body shaking": "seizures",
+  "losing control": "seizures",
+
+  // Weight loss
+  "losing weight": "weight loss",
+  "getting thinner": "weight loss",
+  "bones showing": "weight loss",
+  "skinnier": "weight loss",
+  "shrinking body": "weight loss",
+
+  // Breathing difficulty
+  "hard to breathe": "breathing difficulty",
+  "heavy breathing": "breathing difficulty",
+  "labored breathing": "breathing difficulty",
+  "panting heavily": "breathing difficulty",
+  "shortness of breath": "breathing difficulty",
+
+  // Itching
+  "scratching a lot": "itching",
+  "itchy skin": "itching",
+  "keeps scratching": "itching",
+  "constant itching": "itching",
+  "rubbing against things": "itching",
+
+  // Ear discharge
+  "ear gunk": "ear discharge",
+  "ear pus": "ear discharge",
+  "dirty ears": "ear discharge",
+  "leaking ears": "ear discharge",
+  "bloody ear": "ear discharge",
+
+  // Pain
+  "crying in pain": "pain",
+  "limping in pain": "pain",
+  "sensitive to touch": "pain",
+  "wincing": "pain",
+  "hurting": "pain",
+
+  // Eye discharge
+  "eye gunk": "eye discharge",
+  "watery eyes": "eye discharge",
+  "goopy eyes": "eye discharge",
+  "mucus eyes": "eye discharge",
+  "teary eyes": "eye discharge",
+
+  // Muscle twitching
+  "body jerking": "muscle twitching",
+  "random twitches": "muscle twitching",
+  "uncontrolled movements": "muscle twitching",
+  "muscle spasms": "muscle twitching",
+  "shaking limbs": "muscle twitching",
+
+  // Muscle paralysis
+  "can't move legs": "muscle paralysis",
+  "frozen limbs": "muscle paralysis",
+  "paralyzed body": "muscle paralysis",
+  "legs not working": "muscle paralysis",
+  "dragging body": "muscle paralysis",
+
+  // Abdominal pain
+  "stomach pain": "abdominal pain",
+  "belly pain": "abdominal pain",
+  "tummy ache": "abdominal pain",
+  "painful abdomen": "abdominal pain",
+  "abdomen hurting": "abdominal pain",
+
+  // Abdominal enlargement
+  "big belly": "abdominal enlargement",
+  "swollen stomach": "abdominal enlargement",
+  "bloated abdomen": "abdominal enlargement",
+  "enlarged tummy": "abdominal enlargement",
+  "fat abdomen": "abdominal enlargement",
+
+  // Bloating
+  "bloated": "bloating",
+  "swollen belly": "bloating",
+  "puffy stomach": "bloating",
+  "distended abdomen": "bloating",
+  "belly swelling": "bloating",
+
+  // Difficulty breathing
+  "trouble breathing": "difficulty breathing",
+  "breathing hard": "difficulty breathing",
+  "struggling to breathe": "difficulty breathing",
+  "labored breaths": "difficulty breathing",
+  "breathless": "difficulty breathing",
+
+  // Dehydration
+  "dry gums": "dehydration",
+  "sunken eyes": "dehydration",
+  "lack of water": "dehydration",
+  "not drinking": "dehydration",
+  "dry mouth": "dehydration",
+
 };
